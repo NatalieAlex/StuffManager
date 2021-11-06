36d3546fc320dea005ec4f57c4a86cc66f80afc7
NatalieAlex <63719932+NatalieAlex@users.noreply.github.com>
2021-11-06 21:12:45 +0400

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import gui.EmployeeAddForm;
+import gui.EmployeeFrame;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        EmployeeFrame frame = new EmployeeFrame();
+    }
+}
